733bf91da0fdca24528e788cba1a18045e66b034
Simon Csongor <simon.csongor2@gmail.com>
2021-11-18 16:13:19 +0100

    modified wrapper

--- Parser/pybind11_wrapper.cpp
+++ Parser/pybind11_wrapper.cpp
@@ -6,8 +6,17 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(caffparser, m) {
-    py::class_<CaffFile>(m, "CaffFile")
+    py::class_<CiffFile>(m, "CiffFile")
         .def(py::init<>())
-        .def_readwrite("a", &CaffFile::a)
-        .def_readwrite("vec", &CaffFile::vec);
+        .def_readwrite("header", &CiffFile::header)
+        .def_readwrite("pixelValues", &CiffFile::pixelValues);
+
+    py::class_<CiffFileHeader>(m, "CiffFileHeader")
+        .def(py::init<>())
+        .def_readwrite("contentSize", &CiffFileHeader::contentSize)
+        .def_readwrite("width", &CiffFileHeader::width)
+        .def_readwrite("height", &CiffFileHeader::height)
+        .def_readwrite("caption", &CiffFileHeader::caption)
+        .def_readwrite("tags", &CiffFileHeader::tags);
 }
+
