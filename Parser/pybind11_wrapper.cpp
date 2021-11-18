da8d85e1bc5813cd71a151b07f773754e2941911
Simon Csongor <simon.csongor2@gmail.com>
2021-11-18 18:14:37 +0100

    full parser wrap and test python code

--- Parser/pybind11_wrapper.cpp
+++ Parser/pybind11_wrapper.cpp
@@ -6,6 +6,34 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(caffparser, m) {
+    py::class_<CaffParser>(m, "CaffParser")
+        .def(py::init<>())
+        .def("parse", &CaffParser::parse, py::return_value_policy::copy);
+
+    py::class_<CaffFile>(m, "CaffFile")
+        .def(py::init<>())
+        .def_readwrite("header", &CaffFile::header)
+        .def_readwrite("credits", &CaffFile::credits)
+        .def_readwrite("animationImages", &CaffFile::animationImages);
+
+    py::class_<CaffAnimationImage>(m, "CaffAnimationImage")
+        .def(py::init<>())
+        .def_readwrite("duration", &CaffAnimationImage::duration)
+        .def_readwrite("ciffImage", &CaffAnimationImage::ciffImage);
+
+    py::class_<CaffCredits>(m, "CaffCredits")
+        .def(py::init<>())
+        .def_readwrite("year", &CaffCredits::year)
+        .def_readwrite("month", &CaffCredits::month)
+        .def_readwrite("day", &CaffCredits::day)
+        .def_readwrite("hour", &CaffCredits::hour)
+        .def_readwrite("minute", &CaffCredits::minute)
+        .def_readwrite("creator", &CaffCredits::creator);
+
+    py::class_<CaffHeader>(m, "CaffHeader")
+        .def(py::init<>())
+        .def_readwrite("numOfCiffs", &CaffHeader::numOfCiffs);
+
     py::class_<CiffFile>(m, "CiffFile")
         .def(py::init<>())
         .def_readwrite("header", &CiffFile::header)
