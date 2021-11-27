6dc42b6496fa306a518d89908809266566d3c853
Németh Krisztián <nemethk@ukatemi.com>
2021-11-27 23:16:07 +0100

    Split init files (and commit user test)

--- Backend/mongoinit/init_db.js
+++ Backend/mongoinit/init_db.js
@@ -1,8 +1,2 @@
 db.createCollection("user")
 db.createCollection("caff_file")
-
-db.createUser({
-    user: "flaskuser",
-    pwd: "aGfuEXJRYwBgDEmo8aXK8wY2sCkwQ9N6",
-    roles: [ { role: "readWrite", db: "CaffDatabase"} ]
-});
