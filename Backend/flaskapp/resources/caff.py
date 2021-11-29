07af9b31d087ae53e0970bf137fda957b2631354
Németh Krisztián <nemethk@ukatemi.com>
2021-11-29 18:44:57 +0100

    Added tests for CAFF upload, small fixes

--- Backend/flaskapp/resources/caff.py
+++ Backend/flaskapp/resources/caff.py
@@ -9,8 +9,10 @@ import os
 
 ALLOWED_EXTENSIONS = {'caff'}
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 class CaffDataApi(Resource):
     @jwt_required()
@@ -32,58 +34,65 @@ class CaffUploadApi(Resource):
     @jwt_required()
     def post(self):
         if 'file' not in request.files:
-            return make_response(jsonify(errorMessage="File not in request"), 400)
+            return make_response(jsonify(errorMessage="file not in request"), 400)
 
         try:
             name = request.form.get('name')
             file = request.files['file']
-
-            if file.filename == '':
-                return make_response(jsonify(errorMessage="No file selected for upload"), 400)
-            if not allowed_file(file.filename):
-                return make_response(jsonify(errorMessage="Invalid file format"), 400)
-
-            uploader = get_current_user()
-
-            caffParser = CaffParser()
-            bytes = file.stream.read()
-            parsed_file = caffParser.parse([b for b in bytes])
-
-            previewAnimationImage = parsed_file.animationImages[0]
-            caffFile = CaffFile(
-                caffName=name,
-                numOfCiffs=parsed_file.header.numOfCiffs,
-                creator=parsed_file.credits.creator,
-                creationDate=datetime(
-                    parsed_file.credits.year,
-                    parsed_file.credits.month,
-                    parsed_file.credits.day,
-                    parsed_file.credits.hour,
-                    parsed_file.credits.minute
-                ),
-                comments=[],
-                uploaderName=uploader.name,
-                caffAnimationImage=CaffAnimationImage(
-                    duration=previewAnimationImage.duration,
-                    width=previewAnimationImage.ciffImage.header.width,
-                    height=previewAnimationImage.ciffImage.header.height,
-                    caption=previewAnimationImage.ciffImage.header.caption,
-                    tags=previewAnimationImage.ciffImage.header.tags
-                )
-            )
-
-            caffFile.save()
-
-            filename = str(caffFile.id)
-            with open(os.path.join(os.environ.get('UPLOAD_FOLDER', filename), 'wb+')) as f:
-                f.write(bytes)
-            # for local testing
-            # with open(os.path.join('./uploads/', filename), 'wb+') as f:
-            #     f.write(bytes)
-
-            return make_response('', 201)
         except AttributeError:
             return make_response(jsonify(errorMessage="invalid parameters"), 400)
+
+        if file.filename == '':
+            return make_response(jsonify(errorMessage="no file selected for upload"), 400)
+        if not allowed_file(file.filename):
+            return make_response(jsonify(errorMessage="invalid file format"), 400)
+
+        uploader = get_current_user()
+
+        caffParser = CaffParser()
+        bytes = file.stream.read()
+
+        try:
+            parsed_file = caffParser.parse([b for b in bytes])
+        except ValueError:
+            return make_response(jsonify(errorMessage="invalid file format"), 400)
+
+        previewAnimationImage = parsed_file.animationImages[0]
+        caffFile = CaffFile(
+            caffName=name,
+            numOfCiffs=parsed_file.header.numOfCiffs,
+            creator=parsed_file.credits.creator,
+            creationDate=datetime(
+                parsed_file.credits.year,
+                parsed_file.credits.month,
+                parsed_file.credits.day,
+                parsed_file.credits.hour,
+                parsed_file.credits.minute
+            ),
+            comments=[],
+            uploaderName=uploader.name,
+            caffAnimationImage=CaffAnimationImage(
+                duration=previewAnimationImage.duration,
+                width=previewAnimationImage.ciffImage.header.width,
+                height=previewAnimationImage.ciffImage.header.height,
+                caption=previewAnimationImage.ciffImage.header.caption,
+                tags=previewAnimationImage.ciffImage.header.tags
+            )
+        )
+
+        caffFile.save()
+
+        filename = str(caffFile.id)
+
+        # for docker
+        with open(os.path.join(os.environ.get('UPLOAD_FOLDER'), filename), 'wb+') as f:
+            f.write(bytes)
+
+        # for local testing
+        # with open(os.path.join('./uploads/', filename), 'wb+') as f:
+        #     f.write(bytes)
+
+        return make_response('', 201)
 
 
 
---
+++ Backend/test/unit/test_caff.py
@@ -0,0 +1,36 @@
+import pytest
+from fixtures import client, token
+from io import BytesIO
+
+
+@pytest.mark.username("testuser")
+@pytest.mark.password("test1234")
+def test_caff_upload(client, token):
+    data = {'name': 'this is a name'}
+
+    resp = client.post("/caff/upload", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 400 and resp.is_json and "file not in request" in resp.json["errorMessage"]
+
+    data["file"] = (BytesIO(b"asdasdasdasdasdasdasdasdas"), "1.caff")
+    resp = client.post("/caff/upload", headers={"Authorization": "Bearer " + token}, content_type='multipart/form-data',
+                       data=data)
+    assert resp.status_code == 400 and resp.is_json and "invalid file format" in resp.json["errorMessage"]
+
+    data["file"] = (BytesIO(b"asdasdasdasdasdasdasdasdas"), "1")
+    resp = client.post("/caff/upload", headers={"Authorization": "Bearer " + token}, content_type='multipart/form-data',
+                       data=data)
+    assert resp.status_code == 400 and resp.is_json and "invalid file format" in resp.json["errorMessage"]
+
+    data["file"] = (BytesIO(b"12345678" * 1024 * 1024 * 60), "1")
+    resp = client.post("/caff/upload", headers={"Authorization": "Bearer " + token}, content_type='multipart/form-data',
+                       data=data)
+    assert resp.status_code == 413
+
+    with open("/var/www/parser/TestFiles/1.caff", "rb") as f:
+        data['file'] = (BytesIO(f.read()), "1.caff")
+    resp = client.post("/caff/upload", headers={"Authorization": "Bearer " + token}, content_type='multipart/form-data',
+                       data=data)
+    print(resp.data)
+    assert resp.status_code == 201
+
+    # TODO: Check if parsed data is correct
