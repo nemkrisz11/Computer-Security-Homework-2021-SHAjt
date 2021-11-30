74ff13542e8d72d06a0fdef1f8876a057b81a2f9
Balint Horvath <h.balint12@gmail.com>
2021-11-30 10:44:08 +0100

    Download and Search tests

--- Backend/test/unit/test_caff.py
+++ Backend/test/unit/test_caff.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 import pytest
+
 from fixtures import client, token
 from io import BytesIO
 from mongoengine import connect
@@ -44,5 +47,34 @@ def test_caff_upload(client, token):
 @pytest.mark.username("testuser")
 @pytest.mark.password("test1234")
 def test_caff_download(client, token):
-    # TODO
-    pass
+    resp = client.get("/caff/download/1", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 404 and resp.is_json and "File does not exist" in resp.json["errorMessage"]
+
+    resp = client.get("/caff/download/61a559807aa83d946960d4f2", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 200
+
+
+@pytest.mark.username("testuser")
+@pytest.mark.password("test1234")
+def test_caff_search(client, token):
+
+    resp = client.get("/caff/search?page=0", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 400
+
+    resp = client.get("/caff/search?page=2", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 400
+
+    resp = client.get("/caff/search?perpage=0", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 400
+
+    resp = client.get("/caff/search?username=Test Creator", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 200
+
+    resp = client.get("/caff/search?uploaderName=testuser", headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 200
+
+    resp = client.get("/caff/search?creationDate=" + str(datetime(2020, 7, 2, 14, 50, 0, 0)), headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 200
+
+    resp = client.get("/caff/search?uploadDate=" + str(datetime(2021,11, 29, 22, 40, 46, 35)), headers={"Authorization": "Bearer " + token})
+    assert resp.status_code == 200
