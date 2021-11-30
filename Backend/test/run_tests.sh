6da9aabcb40f9a6d0259b95ef895b0c771d7ac64
Németh Krisztián <nemethk@ukatemi.com>
2021-11-30 15:38:30 +0100

    Report path fix

--- Backend/test/run_tests.sh
+++ Backend/test/run_tests.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ge 1 ] && [ "$1" = "--stdout" ]; then
   python3 -m pytest -s -v "${FLASKTEST_PATH}"
 elif [ "$#" -ge 1 ] && [ "$1" = "--cov" ]; then
-  python3 -m pytest -v --cov --cov-report=xml:/tmp/coverage.xml --cov-report=html:/tmp/htmlcov "${FLASKTEST_PATH}"
+  python3 -m pytest -v --cov --cov-report=xml:/tmp/coverage-reports/coverage.xml --cov-report=html:/tmp/coverage-reports/htmlcov "${FLASKTEST_PATH}"
 else
   python3 -m pytest -v "${FLASKTEST_PATH}"
 fi
